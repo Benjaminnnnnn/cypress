96fc24c6da2fe3dfe7df600218852505e6ed61f1
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2024-02-05 23:17:08 -0800

    added logger

---
+++ src/lib/logger/logger.ts
@@ -0,0 +1,22 @@
+import winston from "winston";
+const { combine, timestamp, json, colorize, simple } = winston.format;
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.File({
+      filename: "app.log",
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: combine(colorize(), simple()),
+    }),
+  );
+}
+
+export { logger };
